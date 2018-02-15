a640e7ad4c8b78c2e221016d4a573b66ef38b88f
lumpirichard <richard.lumpi@sbg.at>
2018-02-15 15:31:55 +0000

    changed from code from NLP to PBE

--- business_logic/nlp/NLP.py
+++ business_logic/nlp/NLP.py
@@ -1,19 +1,1 @@
-import json
-
-user_input = "What is the share price of Apple?"
-
-
-request = {}
-
-req1 = {
-  'type': 'request',
-  'keyword': 'Microsoft'
-}
-
-s = json.dumps(req1)
-
-print(s)
-with open("/Users/richardlumpi/Documents/A Warwick/Year 1/"
-          + "06_Software_Eng/project/CS261_coursework_server/business_logic/nlp/request.txt", "w") as f:
-  f.write(s)
 
