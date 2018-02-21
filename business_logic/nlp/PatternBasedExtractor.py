f35cb08ac9151736d5708ade4b041a43e1093f9f
Maxim <ninexty@gmail.com>
2018-02-21 19:32:00 +0000

    Itendation

--- business_logic/nlp/PatternBasedExtractor.py
+++ business_logic/nlp/PatternBasedExtractor.py
@@ -4,22 +4,23 @@ import re
 
 class PatternBasedExtractor():
 
- patterns = ["price", "news"]
+    patterns = ["price", "news"]
 
- @classmethod
- def get_meaning(cls, string):
-  for p in PatternBasedExtractor.patterns:
-    if p in string:
-      request = {}
-      req1 = {
-        'type': 'request',
-        'subtype': p,
-        'keyword': re.sub(r'[^\w\s]','',string.rsplit(' ', 1)[1])
-      }
-      s = json.dumps(req1)
-      print(s)
+    @classmethod
+    def get_meaning(cls, string):
+        for p in PatternBasedExtractor.patterns:
+            if p in string:
+                request = {}
+                req1 = {
+                    'type': 'request',
+                    'subtype': p,
+                    'keyword': re.sub(r'[^\w\s]','',string.rsplit(' ', 1)[1])
+                }
+                s = json.dumps(req1)
+                print(s)
+
 
 if __name__ == '__main__':
-  PatternBasedExtractor.get_meaning("What is the price of Apple?")
-  PatternBasedExtractor.get_meaning("What is the latest news of Microsoft?")
+    PatternBasedExtractor.get_meaning("What is the price of Apple?")
+    PatternBasedExtractor.get_meaning("What is the latest news of Microsoft?")
 
